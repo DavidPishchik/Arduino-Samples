400b93b82fa3bae5a87859ce3711ef27ac6c64cd
David Pishchik <davidpishchik@gmail.com>
2019-03-06 08:46:48 -0500

    setup project fritzing / ac automated cover btn control

--- tempature_sensor_setup/tempature_sensor_setup.ino
+++ tempature_sensor_setup/tempature_sensor_setup.ino
@@ -6,16 +6,17 @@
 #include <DHT.h>
 
 //Constants
-#define DHTPIN 8     // what pin we're connected to
-#define DHTTYPE DHT22  // DHT 22  (AM2302)
+#define PLUSTEMPPIN 1     
+#define MINUSTEMPPIN 2    
+#define DHTPIN 8     // Define DHT Pin 
+#define DHTTYPE DHT22  // Define DHT 22  (AM2302) 
 DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 
 
-
 //Variables
-int chk;
 float hum;  //Stores humidity value
 float temp; //Stores temperature value
+int baseTemp = 20;
 
 
 void setup() {
@@ -25,18 +26,47 @@ void setup() {
 
 }
 
+
+
 void loop() {
 
     delay(2000);
     //Read data and store it to variables hum and temp
     hum = dht.readHumidity();
     temp= dht.readTemperature();
+
+    if (PLUSTEMPPIN == HIGH){
+      baseTemp++;
+    }
+    if (MINUSTEMPPIN == HIGH){
+       baseTemp--;
+    }
+    
     //Print temp and humidity values to serial monitor
     Serial.print("Humidity: ");
     Serial.print(hum);
     Serial.print(" %, Temp: ");
     Serial.print(temp);
-    Serial.println(" Celsius");
+    Serial.print(" Celsius");
+    Serial.print(" %,  Base Temp: ");
+    Serial.println(baseTemp);
+       
+    if(hotTemp(temp, baseTemp)){
+         // do something   
+         Serial.print(" %,  Base Temp: ");
+         Serial.print(baseTemp);
+         Serial.print(" %,  Hot Temp: ");
+    }
     delay(1000); //Delay 2 sec.
-
 }
+
+boolean hotTemp(int temp, int baseTemp)
+{
+   boolean bigger = false;
+
+   if (temp > baseTemp){
+      bigger = true;
+   }
+   return bigger; 
+}
+
